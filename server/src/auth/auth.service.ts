ba5b4368c922a329d7cda1e713110140aef421ec
Theodore <uzuntudor@yahoo.com>
2025-07-16 20:45:29 +0300

    initiate project, auth register, jwt strategy config

---
+++ server/src/auth/auth.service.ts
@@ -0,0 +1,62 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { AuthDto } from './dto/auth.dto';
+import { hash } from 'argon2';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/client';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+  ) {}
+  async register(dto: AuthDto) {
+    const { email, password } = dto;
+    const oldUser = await this.prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (oldUser) throw new BadRequestException('User already exists');
+
+    const user = await this.prisma.user.create({
+      data: {
+        email,
+        // name,
+        // avatarPath,
+        // phone,
+        password: await hash(password),
+      },
+    });
+
+    const tokens = await this.issueTokens(user.id);
+
+    return {
+      user: this.returnUserFields(user),
+      ...tokens,
+    };
+  }
+
+  private async issueTokens(userId: string) {
+    const data = { id: userId };
+
+    const accessToken = this.jwt.sign(data, {
+      expiresIn: '1h',
+    });
+
+    const refreshToken = this.jwt.sign(data, {
+      expiresIn: '7d',
+    });
+
+    return { accessToken, refreshToken };
+  }
+
+  private async returnUserFields(user: User) {
+    return {
+      id: user.id,
+      email: user.email,
+    };
+  }
+}
