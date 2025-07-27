f1a300971c524e70ca2c91788496bdaa2ba5a9f2
Theodore <uzuntudor@yahoo.com>
2025-07-28 00:02:56 +0300

    order and stripe connect for server

--- server/src/order/order.service.ts
+++ server/src/order/order.service.ts
@@ -1,4 +1,86 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { returnProductObject } from 'src/product/return-product.object';
+import Stripe from 'stripe';
+import { OrderDto } from './dto/order.dto';
 
 @Injectable()
-export class OrderService {}
+export class OrderService {
+  private stripe: Stripe;
+
+  constructor(private prisma: PrismaService) {
+    this.stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '');
+  }
+
+  getAll = async () => {
+    return this.prisma.order.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+      include: {
+        items: {
+          include: {
+            product: {
+              select: returnProductObject,
+            },
+          },
+        },
+      },
+    });
+  };
+
+  getByUserId = async (userId: string) => {
+    return this.prisma.order.findMany({
+      where: {
+        userId,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+      include: {
+        items: {
+          include: {
+            product: {
+              select: returnProductObject,
+            },
+          },
+        },
+      },
+    });
+  };
+
+  placeOrder = async (dto: OrderDto, userId: string) => {
+    const total = dto.items.reduce(
+      (acc, item) => acc + item.price * item.quantity,
+      0,
+    );
+
+    if (total < 1) {
+      throw new Error('Amount must be at least 1 usd');
+    }
+
+    const order = await this.prisma.order.create({
+      data: {
+        items: {
+          create: dto.items,
+        },
+        total,
+        user: {
+          connect: { id: userId },
+        },
+      },
+    });
+
+    const totalInCents = Math.round(total * 100);
+    const paymentIntent = await this.stripe.paymentIntents.create({
+      amount: totalInCents,
+      currency: 'usd',
+      automatic_payment_methods: {
+        enabled: true,
+      },
+      description: `Order by userId ${order.userId}`,
+    });
+
+    return { clientSecret: paymentIntent.client_secret };
+  };
+}
